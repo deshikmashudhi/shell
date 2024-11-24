24c6976583e89b7f47cd1809a85cbbb5822e044c
deshikmashudhi <deshikmashudhi@gmail.com>
2024-11-24 10:50:32 +0530

    first

---
+++ var.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+echo "$PERSON1: hi $PERSON2, good morning"
+echo "$PERSON2: good morning $PERSON1"
+echo "$PERSON1: how are you $PEROSON2"
+echo "$PERSON2: i am good, how are you $PERSON1"
